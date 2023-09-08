13c6d970b02d96031d59be8f7b59b6d98a8005cc
MauricioOliveir4 <mauoliveirasilva049@gmail.com>
2023-09-08 15:02:39 -0300

    Create prova do bill media de notas.c

---
+++ prova do bill media de notas.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+    int main(){
+    	
+        float alunoa, alunob, media;
+            printf("Digite a nota do aluno A: ");
+        scanf("%f", &alunoa);
+            printf("Digite a nota do aluno B: ");
+        scanf("%f", &alunob);
+
+        media = (alunoa + alunob) / 2;
+
+    printf("A media eh : %f",media);
+        return 0;
+    };
+    
